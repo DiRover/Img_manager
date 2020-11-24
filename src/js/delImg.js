6049e12e300357bf3482f2fb116bdb0b11050989
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-11-24 22:40:32 +0300

    v1.0

---
+++ src/js/delImg.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-cycle */
+import { url } from './app';
+
+const showFiles = document.querySelector('[data-srv="show-files"]');
+
+// удаляем картинки с сервера
+export default async function delImg(e) {
+  const { target } = e;
+  if (!target.hasAttribute('data-del')) return; // если не попали на крестик выходим
+  showFiles.innerHTML = '';
+  const parent = target.closest('[data-box="img"]'); // находим контейнер с картинкой и Х
+  console.log(parent);
+  const img = target.previousSibling; // находим картинку
+  const src = img.getAttribute('src'); // получаем значение атрибута src
+  console.log(src);
+  console.log(`${src}`)
+  const response = await fetch(`${src}`, {
+    method: 'DELETE',
+  });
+  const result = await response.text();
+  console.log(result);
+  showFiles.innerHTML = result;
+  
+  parent.remove(); // удаляем контейнер с картинкой и Х
+}
